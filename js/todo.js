450e9cf02cd6bd280af36ae36aa6d90b29d75208
bril-liant <lizseion@gmail.com>
2021-09-23 07:13:33 +0900

    4

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem.apply("todos", JSON.stringify(toDos));
+}
+
+function deleteTodo() {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   //   console.log("i will paint", newTodo);
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   toDoList.appendChild(li);
 }
@@ -15,6 +30,7 @@ function handleToDOSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
 }
 
