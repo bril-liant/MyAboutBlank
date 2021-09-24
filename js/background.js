91ae6752cec74212181e4ec930d2573f9903ee60
bril-liant <lizseion@gmail.com>
2021-09-25 07:46:00 +0900

    my about black

--- js/background.js
+++ js/background.js
@@ -1,22 +1,35 @@
 const images = [
-  "https://pbs.twimg.com/media/ENbTFEfVUAEcft6.jpg:orig",
-  "https://pbs.twimg.com/media/EWw7EKqXkAQzf08.jpg:orig",
   "https://pbs.twimg.com/media/EqkfJeyVkAA8_eK.jpg:orig",
-  "https://pbs.twimg.com/media/D9OipFwVUAA9YYc.jpg:orig",
   "https://pbs.twimg.com/media/EX-v445VAAAbXhw.jpg:orig",
   "https://pbs.twimg.com/media/EkSPgwkUcAIb3os.jpg:orig",
-  "https://pbs.twimg.com/media/EaOzUtJWsAAp1df.jpg:orig",
   "https://pbs.twimg.com/media/EzlL7qnUcAIpvo8.jpg:orig",
-  "https://pbs.twimg.com/media/ElgWXJ5VoAIkwWX.jpg:orig",
   "https://pbs.twimg.com/media/Ej5G7PMVkAEJE05.jpg:orig",
   "https://pbs.twimg.com/media/EojYN1SUYAI_7Xd.jpg:orig",
-  "https://pbs.twimg.com/media/E9TIz-CUUAAkTby.jpg:orig",
+  "https://pbs.twimg.com/media/E1RPlkjVIAUHY7e.jpg:orig",
+  "https://pbs.twimg.com/media/EqFwGZBUUAAUJBB.jpg:orig",
+  "https://pbs.twimg.com/media/Ejacqp9UwAA2U8i.jpg:orig",
+  "https://pbs.twimg.com/media/ErRx97eU0AEmuhD.jpg:orig",
+  "https://pbs.twimg.com/media/Epq1IQBVQAATzaS.jpg:orig",
+  "https://pbs.twimg.com/media/E8lfdcdUUAQx5jW.jpg:orig",
+  "https://pbs.twimg.com/media/EkNFFaDVoAApZbW.jpg:orig",
+  "https://pbs.twimg.com/media/EpB9rBXUcActF7w.jpg:orig",
+  "https://pbs.twimg.com/media/E8u_-fFVcAE-tT0.jpg:orig",
+  "https://pbs.twimg.com/media/E9EJ23uVkAENj5Y.jpg:orig",
+  "https://pbs.twimg.com/media/ElkLzHiVgAIoKLV.jpg:orig",
+  "https://pbs.twimg.com/media/E0x786-UYAAd8w2.jpg:orig",
+  "https://pbs.twimg.com/media/E29zVcZVoAAvslz.jpg:orig",
+  "https://pbs.twimg.com/media/E29zVcnVEAYubAK.jpg:orig",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+// bgImage.id = "bgImg";
 
 bgImage.src = chosenImage;
 
-document.body.appendChild(bgImage);
+document.documentElement.style.setProperty(
+  `--background-image`,
+  `url(${chosenImage})`
+);
+// document.body.appendChild(bgImage);
